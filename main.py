c0be33533228c97e77a13924420c04be37e1996c
Yash Raj <ryash072007@gmail.com>
2024-08-17 01:07:09 +0400

    Project Features Added

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+The main file for 12th Grade Computer Project 2024 for:
+1. Yash
+2. Atharv
+
+Project Idea: Banking System
+
+Features:
+1. User Authentication -> Username and Password
+2. Multiple Account Types and Account Creation
+    i. Savings Account -> Simple Interest Calculation
+    ii. Checkings Account -> Basic deposition and withdrawal functions without interest
+3. Simple Interest Calculation:
+    i. View interest gathered over time
+    ii. Simple one for Savings account and can only take out certain amount of times a month
+4. Basic Fund Transfer:
+    i. Allow transfer within the same user -> list self option
+    ii. Allow transfer between different users
+5. Loan Management:
+    i. Loan Applications -> Make your own loan with different conditions for different interest rates
+    ii Loan Repayment -> Users can make manual repayments and the system keeps track of it
+6. Account Statement:
+    i. User can view all the transations that have been processed
+    ii. User can sort through different type of transactions
+    
+'''
